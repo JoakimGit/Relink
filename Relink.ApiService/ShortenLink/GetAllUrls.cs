0a79f2b4afadcc88a5552b1a401dc94dbe9b6664
Joakim <jokke34@gmail.com>
2025-06-22 23:18:20 +0200

    Added endpoints for getting original url and getting all created links. Also need work

---
+++ Relink.ApiService/ShortenLink/GetAllUrls.cs
@@ -0,0 +1,16 @@
+
+namespace Relink.ApiService.ShortenLink;
+
+public class GetAllUrls : IEndpoint
+{
+    public static void Map(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/urls", Handle).WithSummary("Gets all shortened URLs");
+    }
+
+    private static async Task<Ok<List<ShortenedLink>>> Handle(AppDbContext database, CancellationToken cancellationToken)
+    {
+        var links = await database.ShortenedLinks.ToListAsync(cancellationToken);
+        return TypedResults.Ok(links ?? []);
+    }
+}
