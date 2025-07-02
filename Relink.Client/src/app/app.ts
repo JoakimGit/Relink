f828ba0f8a69189e90dd082b9e4e392bf412a0e9
Joakim <jokke34@gmail.com>
2025-07-02 17:21:27 +0200

    Added angular client app with tailwind.

---
+++ Relink.Client/src/app/app.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  imports: [],
+  template: ` <main class="text-2xl text-red-400">Hello World</main> `,
+})
+export class App {
+  protected title = "Relink.Client";
+}
